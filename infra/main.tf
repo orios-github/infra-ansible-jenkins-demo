d3de899c8bf1f63e752cad1190c1752f5d13948a
orios-github <135855283+orios-github@users.noreply.github.com>
2026-02-05 12:56:36 -0600

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,1 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
 
+resource "aws_instance" "web" {
+  count = 3
+  ami = "ami-0c55b159cbfafe1f0" # Ubuntu 20.04 (ejemplo)
+  instance_type = "t2.micro"
+
+
+  tags = {
+    Name = "web-${count.index}"
+  }
+}
+
+output "web_ips" {
+  value = aws_instance.web[*].public_ip
+}
