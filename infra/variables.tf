44c25f9493cc6e8680966e4277609dcd297251a9
orios-github <135855283+orios-github@users.noreply.github.com>
2026-02-07 21:49:15 -0600

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,15 +1,20 @@
+#Define AWS region to deploy servers.
 variable "region" {
   default = "us-east-1"
 }
 
+#Define EC2 instance type.
 variable "instance_type" {
   default = "t3.micro"
 }
 
+#Number of servers to create.
 variable "server_count" {
   default = 3
 }
 
+#Define AMI (SO Image).
 variable "ami_id" {
-  default = "ami-0786adace1541ca80"
+  #Ubuntu 24.04 LTS
+  default = "ami-0786adace1541ca80" 
 }
