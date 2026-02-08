ffc3d8a8cbafc5ca9d6a28d1c31eaefdb5e11a71
orios-github <135855283+orios-github@users.noreply.github.com>
2026-02-07 23:35:53 -0600

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -3,11 +3,18 @@ provider "aws" {
   region = var.region
 }
 
+#Resource access to public key
+resource "aws_key_pair" "jenkins_key" {
+  key_name   = "jenkins-key"
+  public_key = file("~/.ssh/id_rsa.pub")   #Public Key
+}
+
 #Resource that creates EC2 instances.
 resource "aws_instance" "web" {
   count = var.server_count    #Servers amount
   ami = var.ami_id    #Image (SO)    
   instance_type = var.instance_type    #Instance type
+  key_name      = aws_key_pair.jenkins_key.key_name
 
   #Tags for each server.
   tags = {
