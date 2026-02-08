f46bb16e9b6c8bdd6afbf3faa1a4921e779ba608
orios-github <135855283+orios-github@users.noreply.github.com>
2026-02-07 21:28:10 -0600

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,10 +2,14 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "instance_type {
+variable "instance_type" {
   default = "t2.micro"
 }
 
 variable "server_count" {
   default = 3
+}
+
+variable "ami_id" {
+  default = "ami-0786adace1541ca80"
 }
