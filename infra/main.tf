de791168360ba00e4b923f9b3a728d815858f694
orios-github <135855283+orios-github@users.noreply.github.com>
2026-02-07 21:35:20 -0600

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,17 +1,20 @@
+#Configure provider and region.
 provider "aws" {
   region = var.region
 }
 
+#Resource that creates EC2 instances.
 resource "aws_instance" "web" {
-  count = var.server_count
-  ami = var.ami_id 
-  instance_type = var.instance_type
+  count = var.server_count    #Servers amount
+  ami = var.ami_id    #Image (SO)    
+  instance_type = var.instance_type    #Instance type
 
-
+  #Tags for each server.
   tags = {
-    Name = "web-${count.index}"
+    Name = "web-${count.index}"   #web-0, web-1, web-2
   }
 }
+
 
 output "web_ips" {
   value = aws_instance.web[*].public_ip
